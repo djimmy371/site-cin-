00424e796ac634f44a40b8dd02a267ce395df53f
djimmy371 <djimmy.allard@hotmail.fr>
2024-03-23 22:45:26 +0100

    add user rating

--- main.js
+++ main.js
@@ -64,3 +64,65 @@ function displayMovieDetails(movieId) {
 
 // Charger les films populaires au chargement de la page
 window.onload = displayPopularMovies;
+
+
+
+function getMovieDetails(movieId) {
+    return fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=e68f4fc7136ec12bfeb833e681ce8f32&language=fr-FR`)
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        })
+        .catch(error => {
+            console.error('Une erreur s\'est produite lors de la récupération des détails du film :', error);
+            return null;
+        });
+}
+
+function getUserRatings(movieId) {
+    return fetch(`https://api.themoviedb.org/3/movie/${movieId}/ratings?api_key=e68f4fc7136ec12bfeb833e681ce8f32`)
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        })
+        .catch(error => {
+            console.error('Une erreur s\'est produite lors de la récupération des évaluations des utilisateurs :', error);
+            return null;
+        });
+}
+
+function displayMovieDetails() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const movieId = urlParams.get('movieId');
+
+    Promise.all([getMovieDetails(movieId), getUserRatings(movieId)])
+        .then(([details, ratings]) => {
+            if (details) {
+                const movieDetailsContainer = document.getElementById('movie-details');
+                if (movieDetailsContainer) {
+                    const img = document.createElement('img');
+                    img.src = `https://image.tmdb.org/t/p/w200${details.poster_path}`;
+                    img.alt = details.title;
+
+                    const title = document.createElement('h1');
+                    title.textContent = details.title;
+
+                    const description = document.createElement('p');
+                    description.textContent = details.overview;
+
+                    const userRating = document.createElement('p');
+                    userRating.textContent = `Note moyenne des utilisateurs : ${ratings ? ratings.average : 'Non disponible'}`;
+
+                    movieDetailsContainer.appendChild(img);
+                    movieDetailsContainer.appendChild(title);
+                    movieDetailsContainer.appendChild(description);
+                    movieDetailsContainer.appendChild(userRating);
+                } else {
+                    console.error('L\'élément movie-details n\'a pas été trouvé.');
+                }
+            }
+        });
+}
+
+// Charger les détails du film au chargement de la page
+window.onload = displayMovieDetails;
