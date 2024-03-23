e15bd79395ccb8cd5bca5a37d3c3c2011aa54fa8
djimmy371 <djimmy.allard@hotmail.fr>
2024-03-23 23:54:18 +0100

    fix

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const apiKey = 'e68f4fc7136ec12bfeb833e681ce8f32';
 
-function fetchUpcomingMovies() {
-    const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=e68f4fc7136ec12bfeb833e681ce8f32`;
+function fetchUpcomingMovies(apiKey) {
+    const today = new Date();
+    const formattedDate = today.toISOString().split('T')[0]; // Extraction de la partie de la date au format YYYY-MM-DD
+    const encodedDate = encodeURIComponent(formattedDate); // Encodage de la date pour inclure dans l'URL
+    const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&primary_release_date.gte=${encodedDate}`;
 
     fetch(url)
         .then(response => response.json())
@@ -12,11 +15,15 @@ function fetchUpcomingMovies() {
             console.error('Error fetching upcoming movies:', error);
         });
 }
-
 function displayUpcomingMovies(movies) {
     const upcomingMoviesContainer = document.getElementById('upcoming-movies');
     upcomingMoviesContainer.innerHTML = ''; // Clear previous content
     
+    if (!movies || movies.length === 0) {
+        upcomingMoviesContainer.textContent = 'Aucun film à venir trouvé.';
+        return;
+    }
+
     movies.forEach(movie => {
         const movieDiv = document.createElement('div');
         movieDiv.classList.add('movie');
@@ -27,11 +34,22 @@ function displayUpcomingMovies(movies) {
         const releaseDate = document.createElement('p');
         releaseDate.textContent = `Release Date: ${movie.release_date}`;
 
+        const poster = document.createElement('img');
+        if (movie.poster_path) {
+            poster.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+            poster.alt = movie.title;
+        } else {
+            poster.src = 'placeholder-image-url.jpg'; // URL de l'image par défaut si aucune image n'est disponible
+            poster.alt = 'No Image Available';
+        }
+
+        movieDiv.appendChild(poster);
         movieDiv.appendChild(title);
         movieDiv.appendChild(releaseDate);
 
         upcomingMoviesContainer.appendChild(movieDiv);
     });
 }
+
 
 fetchUpcomingMovies();
